28d9adfc3f1e724c6a0d7e6cdc9c92ff6e17f564
SalimBXL <el.salim.salim@gmail.com>
2023-11-11 16:24:05 +0100

    Authentication system

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+    has_secure_password
+
+    validates :email, presence: true
+    normalizes :email, with: ->(email) {email.strip.downcase}
+
+    validates :firstname, presence: true
+    normalizes :firstname, with: ->(firstname) {firstname.strip.capitalize}
+
+    validates :lastname, presence: true
+    normalizes :lastname, with: ->(lastname) {lastname.strip.capitalize}
+
+    generates_token_for :password_reset, expires_in: 15.minutes do
+        password_salt&.last(10)
+    end
+
+    generates_token_for :email_confirmation, expires_in: 24.hours do
+        email
+    end
+end
