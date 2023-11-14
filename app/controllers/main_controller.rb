bde368e99a7bc9b64addbfd85c8a567bc6620a37
SalimBXL <el.salim.salim@gmail.com>
2023-11-14 01:52:38 +0100

    dashboard suite

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -10,9 +10,56 @@ class MainController < ApplicationController
         delta = 0 if (dayOfWeek == 1)
         delta = (dayOfWeek - 1) if (dayOfWeek > 1)
         @firstDayOfCalendar = firstDayOfTheYear - delta.days 
+
+        data = {
+            "2023-01-19": { 
+                date: "2023-01-19", 
+                job: {
+                    code: "",
+                    ampm: true
+                }, 
+                absence: {
+                    code: "co", 
+                    valide: true
+                }
+            },
+            "2023-02-14": { 
+                date: "2023-02-14", 
+                job: {
+                    code: "5",
+                    ampm: true
+                }, 
+                absence: {
+                    code: "", 
+                    valide: false
+                }
+            },
+            "2023-02-21": { 
+                date: "2023-02-21", 
+                job: {
+                    code: "fdg",
+                    ampm: false
+                }, 
+                absence: {
+                    code: "", 
+                    valide: false
+                }
+            },
+            "2023-03-24": { 
+                date: "2023-03-24", 
+                job: {
+                    code: "",
+                    ampm: true
+                }, 
+                absence: {
+                    code: "co", 
+                    valide: false
+                }
+            }
+        }
+        @data = HashWithIndifferentAccess.new(data)
     end
 
     private
-
     
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+    def getHeatMapColor(data)
+        res = "blue" if data[:job][:code].present? && data[:job][:ampm]
+        res = "lightblue" if data[:job][:code].present? && !data[:job][:ampm]
+        res = "red" if data[:absence][:code].present? && !data[:absence][:valide]
+        res = "green" if data[:absence][:code].present? && data[:absence][:valide]
+        return res
+    end
 end
